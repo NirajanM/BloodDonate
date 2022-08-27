75faa9c1415780fee9ef29e39046970a68c6ed90
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-27 23:20:53 +0545

    first section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,26 +23,26 @@ const App: React.FunctionComponent = () => {
   const CurrentSection: React.FunctionComponent = allPages[currentPage as keyof typeof allPages];
   return (
     <>
-      <nav className='container bg-red-400 h-10 text-white flex justify-center items-center'>
-        <ul className='flex gap-x-6 items-center'>
+      <nav className='container bg-red-400 h-14 text-white flex justify-center items-center'>
+        <ul className='flex gap-x-6 items-center text-lg font-semibold'>
           <li
             onClick={() => {
               setCurrentPage("home");
             }}
-            className={currentPage === "home" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+            className={currentPage === "home" ? "scale-125 font-black cursor-pointer underline" : "cursor-pointer"}
           >Home</li>
 
           <li
             onClick={() => {
               setCurrentPage("about");
             }}
-            className={currentPage === "about" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+            className={currentPage === "about" ? "scale-125 font-black underline cursor-pointer" : "cursor-pointer"}
           >About Us</li>
 
           <li onClick={() => {
             setCurrentPage("contact");
           }}
-            className={currentPage === "contact" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+            className={currentPage === "contact" ? "scale-125 font-black underline cursor-pointer" : "cursor-pointer"}
           >Contact Us</li>
         </ul>
       </nav>
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
-
+import Background from '../img/donateBlood.png'
 interface IHomeProps {
 }
 
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
     return (
-        <div className='container h-screen text-center'>
-            this is home page
+        <div className='container'>
+            <div className="container flex flex-col lg:flex-row justify-around">
+                <div className='text-left text-slate-600 py-10 sm:py-20 md:py-28 lg:py-40 text-3xl font-bold md:text-4xl lg:text-5xl px-4'>
+                    <h1>The gift of blood is a gift to someone’s life.</h1>
+                </div>
+                <div
+                    className='bg-contain bg-center bg-no-repeat lg:w-7/12'
+                    style={{
+                        backgroundImage: `url(${Background})`
+                    }}
+                ></div>
+            </div>
+
         </div>
     );
 };
