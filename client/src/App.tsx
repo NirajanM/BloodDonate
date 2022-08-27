07d0a28b1770af8b2eddb258d9f9dedd42cbe98b
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-27 21:36:10 +0545

    updated color scheme and justifyed content better

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import './App.css';
 import Footer from './components/Footer';
-import Header from './components/Header';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
@@ -24,32 +23,30 @@ const App: React.FunctionComponent = () => {
   const CurrentSection: React.FunctionComponent = allPages[currentPage as keyof typeof allPages];
   return (
     <>
-      <header className='container bg-blue-300 text-white'>
-        <nav className='flex justify-center items-center'>
-          <ul className='flex justify-around space-x-8 items-center'>
-            <li
-              onClick={() => {
-                setCurrentPage("home");
-              }}
-              className={currentPage === "home" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
-            >Home</li>
-
-            <li
-              onClick={() => {
-                setCurrentPage("about");
-              }}
-              className={currentPage === "about" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
-            >About Us</li>
-
-            <li onClick={() => {
-              setCurrentPage("contact");
+      <nav className='container bg-red-400 h-10 text-white flex justify-center items-center'>
+        <ul className='flex gap-x-6 items-center'>
+          <li
+            onClick={() => {
+              setCurrentPage("home");
             }}
-              className={currentPage === "contact" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
-            >Contact Us</li>
+            className={currentPage === "home" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+          >Home</li>
 
-          </ul>
-        </nav>
-      </header>
+          <li
+            onClick={() => {
+              setCurrentPage("about");
+            }}
+            className={currentPage === "about" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+          >About Us</li>
+
+          <li onClick={() => {
+            setCurrentPage("contact");
+          }}
+            className={currentPage === "contact" ? "scale-125 font-black cursor-pointer" : "cursor-pointer"}
+          >Contact Us</li>
+        </ul>
+      </nav>
+
       <CurrentSection />
       <Footer />
     </>
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Footer: React.FunctionComponent = () => {
     return (
         <>
-            <footer className='container bg-blue-300 text-white text-center'>
+            <footer className='container bg-red-400 h-10 text-white text-center'>
                 this is a footer
             </footer>
         </>
