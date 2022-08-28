948efd24f82337049cf9c118b495cb0c55b29a09
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-28 13:05:41 +0545

    designed footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-
+const currentYear: number = new Date().getFullYear();
 const Footer: React.FunctionComponent = () => {
     return (
         <>
-            <footer className='container bg-red-400 h-10 text-white text-center'>
-                this is a footer
+            <footer className='container bg-red-400 h-40 text-white flex justify-center items-center'>
+                <span>Designed and Developed by <a className='font-bold text-lg' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a> All rights reserved © {currentYear}.</span>
+
             </footer>
         </>
     );
