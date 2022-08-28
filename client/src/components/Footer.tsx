aecaa4116bd1d97ba5c58ee76468321c763f5263
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-28 13:20:38 +0545

    updated footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import Logo from '../img/highResLogo.png';
 const currentYear: number = new Date().getFullYear();
 const Footer: React.FunctionComponent = () => {
     return (
         <>
-            <footer className='container bg-red-400 h-40 text-white flex justify-center items-center'>
-                <span>Designed and Developed by <a className='font-bold text-lg' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a> All rights reserved © {currentYear}.</span>
+            <footer className='container bg-red-400 h-44 text-white flex justify-center flex-col items-center'>
+                <img className="w-24" src={Logo} alt="logo" />
+                <span>Designed and Developed by <a className='font-bold text-lg' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a></span>
+                <span>© {currentYear}, All rights reserved.</span>
 
             </footer>
         </>
