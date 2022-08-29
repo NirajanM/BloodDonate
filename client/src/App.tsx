8f7cb3e2603b2060bb0e21a6d2dd5553f79fb7d4
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-29 08:51:56 +0545

    implemented react router instead of logic that i did back then

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -24,33 +25,35 @@ const App: React.FunctionComponent = () => {
   const CurrentSection: React.FunctionComponent = allPages[currentPage as keyof typeof allPages];
   return (
     <>
-      <nav className='container bg-red-400 h-20 text-white flex justify-end pr-1 md:pr-4 lg:pr-8 items-center'>
-        <div className='absolute left-6 top-2'>
-          <img src={Logo} className="w-16" alt="logo" onClick={() => {
-            setCurrentPage("home");
-          }} />
-        </div>
-        <ul className='flex gap-x-6 items-center text-lg font-semibold'>
-          <li
-            onClick={() => {
+      <Router>
+        <nav className='container bg-red-400 h-20 text-white flex justify-end pr-1 md:pr-4 lg:pr-8 items-center'>
+          <div className='absolute left-6 top-2'>
+            <img src={Logo} className="w-16" alt="logo" onClick={() => {
               setCurrentPage("home");
-            }}
-            className={currentPage === "home" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
-          >Home</li>
-          <li
-            onClick={() => {
-              setCurrentPage("about");
-            }}
-            className={currentPage === "about" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
-          >About Us</li>
+            }} />
+          </div>
+          <ul className='flex gap-x-6 items-center text-lg font-semibold'>
 
-          <li onClick={() => {
-            setCurrentPage("contact");
-          }}
-            className={currentPage === "contact" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
-          >Contact Us</li>
-        </ul>
-      </nav>
+            <li
+              className={currentPage === "home" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
+            ><Link to="/">Home</Link></li>
+
+            <li
+              className={currentPage === "about" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
+            ><Link to="/about">About Us</Link></li>
+
+            <li
+              className={currentPage === "contact" ? "scale-125 border rounded-sm p-1 bg-slake-200 font-black cursor-pointer hover:bg-slate-300" : "cursor-pointer hover:bg-slate-300 hover:rounded-sm"}
+            ><Link to="/contact">Contact Us</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Router>
+
 
       <CurrentSection />
       <Footer />
