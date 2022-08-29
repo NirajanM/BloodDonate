3bb92c2e227d2eacbbf64496e48b5167a1a71203
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-29 11:15:36 +0545

    responsiveness of text and footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,9 +4,9 @@ const currentYear: number = new Date().getFullYear();
 const Footer: React.FunctionComponent = () => {
     return (
         <>
-            <footer className='container bg-red-400 h-60 text-white flex justify-around flex-col items-center'>
-                <img className="w-24" src={Logo} alt="logo" />
-                <span>Designed and Developed by <a className='font-bold text-lg' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a></span>
+            <footer className='text-xs sm:text-lg container bg-red-400 h-60 text-white flex justify-around flex-col items-center'>
+                <img className="w-20 sm:w-24" src={Logo} alt="logo" />
+                <span>Designed and Developed by <a className='font-bold' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a></span>
                 <span>© {currentYear}, All rights reserved.</span>
                 <div></div>
             </footer>
