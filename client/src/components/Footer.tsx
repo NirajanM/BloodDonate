9b469c0b480f5d1a25aaa8ce376a94c816f509ca
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-30 10:15:43 +0545

    fixed linkedin profile prob in footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FunctionComponent = () => {
         <>
             <footer className='text-xs sm:text-lg container bg-red-400 h-60 text-white flex justify-around flex-col items-center'>
                 <img className="w-20 sm:w-24" src={Logo} alt="logo" />
-                <span>Designed and Developed by <a className='font-bold' href="https://https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a></span>
+                <span>Designed and Developed by <a className='font-bold' href="https://www.linkedin.com/in/nirajan-malla-36a111210/">Nirajan Malla.</a></span>
                 <span>© {currentYear}, All rights reserved.</span>
                 <div></div>
             </footer>
