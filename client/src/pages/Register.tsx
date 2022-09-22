20d7cf80d4e3de10e5b8024f6a6e7c1bcf18b94b
Nirajan Malla <nirajannitro5@gmail.com>
2022-09-22 20:12:07 +0545

    worked on registration page, submit action left to work

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface IRegisterProps {
 }
 
 const Register: React.FunctionComponent<IRegisterProps> = (props) => {
+    const [focusedAddress, setFocusedAddress] = useState<boolean>(false);
     return (
-        <div className="h-screen flex">
-            <div className="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center">
+        <div className="h-screen flex ">
+
+            <div className="hidden md:flex w-1/2 bg-gradient-to-r from-red-400/30 to-transparent i justify-around items-center">
                 <div>
-                    <h1 className="text-white font-bold text-4xl font-sans">GoFinance</h1>
-                    <p className="text-white mt-1">The most popular peer to peer lending at SEA</p>
-                    <button type="submit" className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button>
+                    <h1 className="text-red-400 font-bold text-4xl font-sans">Register</h1>
+                    <p className="text-black mt-1">for a chance to be a real life hero</p>
                 </div>
             </div>
-            <div className="flex w-1/2 justify-center items-center bg-white">
-                <form className="bg-white">
-                    <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
-                    <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
-                    <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                        </svg>
-                        <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+            <div className="flex w-1/2 justify-center items-center">
+                <form className='text-center '>
+                    <h1 className="text-red-400 font-bold text-2xl mb-1">Please Be Genuine!</h1>
+                    <p className="text-sm font-normal text-gray-600 mb-7">we don't want fake details</p>
+                    <input className="border-2 py-2 px-3 rounded-2xl mb-4 block text-center outline-none" type="text" placeholder="your name" />
+                    <label htmlFor="cars">Blood Group: </label>
+                    <select className="mb-2" id="cars" name="cars">
+                        <option value="A+">A+</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B-">B-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
+                        <option value="O+">O+</option>
+                        <option value="O-">O-</option>
+                    </select>
+
+                    <div className='text-gray-600 px-4'>
+                        <span>Gender:</span>
+                        <div className='flex justify-around'>
+                            <div>
+                                <input type="radio" id="male" name="gender" value="M" />
+                                <label htmlFor="male">M</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="female" name="gender" value="60" />
+                                <label htmlFor="female">F</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="other" name="gender" value="60" />
+                                <label htmlFor="other">O</label>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                        </svg>
-                        <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
-                    </div>
-                    <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
-                    <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+                    <input className="border-2 py-2 px-3 rounded-2xl mt-4 block text-center outline-none" type="text" placeholder="birthyear in AD" />
+                    <input className="border-2 py-2 px-3 rounded-2xl mt-4 block text-center outline-none" type="tel" placeholder="phone number" />
+                    <input
+                        onFocus={() => { setFocusedAddress(true) }}
+                        onBlur={() => { setFocusedAddress(false) }}
+                        className="border-2 py-2 px-3 mt-4 rounded-2xl block text-center outline-none" type="text" placeholder="your address" />
+                    {focusedAddress && <span className='text-sm text-slate-400'>format: devchuli-16,Nawalpur</span>}
+                    <button type="submit" className="block w-full bg-red-400 hover:bg-red-500 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">submit</button>
                 </form>
             </div>
         </div>
