3ba405383ee40e7b0a4241d01195b9b57246d613
Nirajan Malla <nirajannitro5@gmail.com>
2022-11-11 11:23:03 +0545

    connected to my email sender api

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -12,13 +12,18 @@ const ContactUs: React.FunctionComponent<ContactUsProps> = (props) => {
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+    const [notice, setNotice] = useState<boolean>(false);
 
 
 
     let makeSendRequest = () => {
         axios.post<tReview>(
-            'http://localhost:4000/review',
-            { name, email, message }
+            "https://email-sender-nine.vercel.app/bloodmail",
+            { name, email, text: message }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
         ).then(
             (res) => {
                 console.log(res);
@@ -26,11 +31,12 @@ const ContactUs: React.FunctionComponent<ContactUsProps> = (props) => {
                 setName("");
                 setEmail("");
                 setMessage("");
-                alert("your message submitted successfully, thank you for contacting us");
+                setNotice(true);
+                setTimeout(() => {
+                    setNotice(false)
+                }, 3500);
             });
-
     }
-
 
     return (
         <div className='h-screen flex items-center justify-center text-center px-4'>
@@ -59,7 +65,7 @@ const ContactUs: React.FunctionComponent<ContactUsProps> = (props) => {
                     setMessage(e.target.value);
                 }} className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-7 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"></textarea>
 
-
+                {notice ? <span className='mb-4'>Delivered Successfully</span> : null}
                 <div className="flex items-center justify-center">
                     <button className="shadow bg-red-400 hover:bg-red-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" onClick={(e) => {
                         e.preventDefault();
